619a65da887543b2199545f639c6e3d1fea1189b
affjljoo3581 <affjljoo3581@gmail.com>
2022-02-17 16:55:18 +0900

    implement text cleaning task

--- bookcover-text-cleaning/src/main.py
+++ bookcover-text-cleaning/src/main.py
@@ -1,15 +1,56 @@
 import argparse
+import os
 
 import cv2
-import easyocr
 import numpy as np
 import pandas as pd
+import tqdm
+
+from masking import BookCoverMask
+
+
+def read_image_by_barcode(barcode: str, base_dir: str) -> np.ndarray:
+    image = cv2.imread(os.path.join(base_dir, *barcode[-3:], f"{barcode}.jpg"))
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    return image
 
 
 def main(args: argparse.Namespace):
-    ...
+    mask = BookCoverMask(args.lower_bounds)
+
+    dataset = pd.read_csv(args.dataset_path, dtype={"barcode": str})
+    barcodes = dataset.barcode.tolist()
+
+    result = []
+    for i in tqdm.trange(0, len(barcodes), args.batch_size):
+        batch_barcodes = barcodes[i : i + args.batch_size]
+        batch_images = [
+            read_image_by_barcode(barcode, args.image_dir) for barcode in batch_barcodes
+        ]
+        batch_masks = mask.generate_masks(batch_images)
+
+        for barcode, image, bbox in zip(batch_barcodes, batch_images, batch_masks):
+            bbox_text = "\n".join(
+                [" ".join([f"{x} {y}" for x, y in bb]) for bb in bbox]
+            )
+            row_item = {
+                "barcode": barcode,
+                "width": image.shape[1],
+                "height": image.shape[0],
+                "bbox": bbox_text,
+            }
+            result.append(row_item)
+
+    pd.DataFrame(result).to_csv(args.output_csv)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+    parser.add_argument("--output-csv", default="bookcover-text-masking.csv")
+    parser.add_argument("--dataset-path", default="../resources/kyobobook-dataset.csv")
+    parser.add_argument("--image-dir", default="../resources/images")
+    parser.add_argument(
+        "--lower-bounds", nargs="+", type=int, default=[0.3, 0.4, 0.5, 0.6]
+    )
+    parser.add_argument("--batch-size", type=int, default=32)
     main(parser.parse_args())
--- bookcover-text-cleaning/src/masking.py
+++ bookcover-text-cleaning/src/masking.py
@@ -3,21 +3,36 @@ import numpy as np
 
 
 class BookCoverMask:
-    def __init__(self):
+    def __init__(self, lower_bounds: list = [0.3, 0.4, 0.5, 0.6]):
         self.reader = easyocr.Reader(["ko", "en"])
+        self.lower_bounds = lower_bounds
+
+    def _generate_masks_with_lower_bound(
+        self, batch_images: list[np.ndarray], lower_bound: float = 0.4
+    ) -> list[list[list[list[int]]]]:
+        n_height, n_width, _ = np.mean([img.shape for img in batch_images], axis=0)
+        batch_results = self.reader.readtext_batched(
+            batch_images, int(n_width), int(n_height), low_text=lower_bound
+        )
+
+        bbox_list = []
+        for image, result in zip(batch_images, batch_results):
+            bbox = [bb for bb, _, _ in result]
+            for bb in bbox:
+                for point in bb:
+                    point[0] = int(point[0] / int(n_width) * image.shape[1])
+                    point[1] = int(point[1] / int(n_height) * image.shape[0])
+            bbox_list.append(bbox)
+        return bbox_list
 
     def generate_masks(
         self, batch_images: list[np.ndarray]
     ) -> list[list[list[list[int]]]]:
-        n_height, n_width, _ = np.mean([img.shape for img in batch_images], dim=0)
-
-        batch_merged_masks = [[] for _ in batch_images]
-        for low_text in np.linspace(0.3, 0.6, 4):
-            batch_results = self.reader.readtext_batched(
-                batch_images, n_width, n_height, low_text=low_text
+        merged_text_masks = [[] for _ in batch_images]
+        for lower_bound in self.lower_bounds:
+            batched_masks = self._generate_masks_with_lower_bound(
+                batch_images, lower_bound
             )
-            for i, result in enumerate(batch_results):
-                for bbox, text, confidence in result:
-                    batch_merged_masks[i].append([[int(x), int(y)] for x, y in bbox])
-
-        return batch_merged_masks
+            for i, text_mask in enumerate(batched_masks):
+                merged_text_masks[i] += text_mask
+        return merged_text_masks
