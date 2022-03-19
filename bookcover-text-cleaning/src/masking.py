d723651388ce9ae93e8d83bb5bde21873798f612
affjljoo3581 <affjljoo3581@gmail.com>
2022-03-20 00:12:27 +0900

    change to use only detector

--- bookcover-text-cleaning/src/masking.py
+++ bookcover-text-cleaning/src/masking.py
@@ -1,12 +1,13 @@
 import easyocr
 import numpy as np
+from easyocr.utils import reformat_input_batched
 
 
 class BookCoverMask:
     def __init__(
         self, scaling_factor: float = 1.0, lower_bounds: list = [0.3, 0.4, 0.5, 0.6]
     ):
-        self.reader = easyocr.Reader(["ko", "en"])
+        self.reader = easyocr.Reader(["ko", "en"], recognizer=False)
         self.scaling_factor = scaling_factor
         self.lower_bounds = lower_bounds
 
@@ -15,18 +16,30 @@ class BookCoverMask:
     ) -> list[list[list[list[int]]]]:
         height, width, _ = np.mean([img.shape for img in batch_images], axis=0)
         height, width = height * self.scaling_factor, width * self.scaling_factor
-        batch_results = self.reader.readtext_batched(
-            batch_images, int(width), int(height), low_text=lower_bound
+
+        batch_images, _ = reformat_input_batched(batch_images, int(width), int(height))
+        batch_horizontal_list, batch_free_list = self.reader.detect(
+            batch_images, low_text=lower_bound, reformat=False
         )
 
-        bbox_list = []
-        for image, result in zip(batch_images, batch_results):
-            bbox = [bb for bb, _, _ in result]
-            for bb in bbox:
-                for point in bb:
+        bbox_list = [[] for _ in batch_images]
+        for i, horizontal_list in enumerate(batch_horizontal_list):
+            if not horizontal_list:
+                continue
+            for x_min, x_max, y_min, y_max in horizontal_list:
+                x_min, x_max = x_min / width, x_max / width
+                y_min, y_max = y_min / height, y_max / height
+                bbox_list[i].append(
+                    [[x_min, y_min], [x_min, y_max], [x_max, y_max], [x_max, y_min]]
+                )
+        for i, free_list in enumerate(batch_free_list):
+            if not free_list:
+                continue
+            for bbox in free_list:
+                for point in bbox:
                     point[0] = point[0] / width
                     point[1] = point[1] / height
-            bbox_list.append(bbox)
+            bbox_list[i].append(free_list)
         return bbox_list
 
     def generate_masks(
