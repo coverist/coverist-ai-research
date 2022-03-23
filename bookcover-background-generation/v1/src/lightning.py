2fa164b14c89a9820d24e2602c9d6dca505a1572
affjljoo3581 <affjljoo3581@gmail.com>
2022-03-24 07:39:13 +0900

    implement the biggan model version 1

---
+++ bookcover-background-generation/v1/src/lightning.py
@@ -0,0 +1,170 @@
+import os
+from typing import Any, Optional
+
+import albumentations as A
+import albumentations.pytorch as AP
+import pandas as pd
+import torch
+import torch.nn as nn
+import torchvision
+from omegaconf import DictConfig
+from pytorch_lightning import LightningDataModule, LightningModule
+from torch.utils.data import DataLoader
+
+from dataset import BigGANDataset
+from modeling import (
+    BigGANDiscriminator,
+    BigGANDiscriminatorConfig,
+    BigGANGenerator,
+    BigGANGeneratorConfig,
+)
+
+try:
+    from apex.optimizers import FusedAdam as Adam
+except ModuleNotFoundError:
+    from torch.optim import Adam
+
+
+class BigGANTrainingModule(LightningModule):
+    def __init__(self, config: DictConfig):
+        super().__init__()
+        self.config = config
+        generator_config = BigGANGeneratorConfig(**config.model.generator)
+        discriminator_config = BigGANDiscriminatorConfig(**config.model.discriminator)
+
+        self.noise_size = generator_config.noise_size
+        self.num_classes = generator_config.num_classes
+
+        self.generator = BigGANGenerator(generator_config)
+        self.generator_ema = BigGANGenerator(generator_config)
+        self.discriminator = BigGANDiscriminator(discriminator_config)
+
+        logging_noise_vectors, logging_labels = self.sample_random_inputs(
+            config.train.logging_images, truncated=True
+        )
+        self.register_buffer("logging_noise_vectors", logging_noise_vectors)
+        self.register_buffer("logging_labels", logging_labels)
+
+    def sample_random_inputs(
+        self, batch_size: Optional[int] = None, truncated: bool = False
+    ) -> tuple[torch.Tensor, torch.Tensor]:
+        batch_size = batch_size or self.config.train.batch_size
+        noise_vector = torch.randn((batch_size, self.noise_size), device=self.device)
+        labels = torch.randint(0, self.num_classes, (batch_size,), device=self.device)
+
+        if truncated:
+            noise_vector = noise_vector.fmod(self.config.model.truncation_threshold)
+        return noise_vector, labels
+
+    def generator_step(self) -> torch.Tensor:
+        noise_vectors, labels = self.sample_random_inputs()
+        loss = -self.discriminator(self.generator(noise_vectors, labels), labels).mean()
+        self.log("train/generator_loss", loss)
+        return loss
+
+    def discriminator_step(
+        self, real_images: torch.Tensor, real_labels: torch.Tensor
+    ) -> torch.Tensor:
+        noise_vectors, fake_labels = self.sample_random_inputs(real_images.size(0))
+        fake_images = self.generator(noise_vectors, fake_labels)
+
+        real_logits = self.discriminator(real_images, real_labels)
+        fake_logits = self.discriminator(fake_images, fake_labels)
+
+        loss = (1 - real_logits).relu().mean() + (1 + fake_logits).relu().mean()
+        self.log("train/discriminator_loss", loss)
+        return loss
+
+    def training_step(
+        self, batch: tuple[torch.Tensor, ...], batch_idx: int, optimizer_idx: int
+    ) -> torch.Tensor:
+        self.generator.requires_grad_(int(optimizer_idx) == 0)
+        self.discriminator.requires_grad_(int(optimizer_idx) == 1)
+
+        if optimizer_idx == 0:
+            return self.generator_step()
+        elif optimizer_idx == 1:
+            return self.discriminator_step(*batch)
+
+    @torch.no_grad()
+    def on_before_zero_grad(self, outputs: torch.Tensor):
+        decay = 0.0
+        if self.global_step > self.config.optim.generator_ema.start_after:
+            decay = self.config.optim.generator_ema.decay
+        for p1, p2 in zip(self.generator_ema.parameters(), self.generator.parameters()):
+            p1.copy_(decay * p1.float() + (1 - decay) * p2.float())
+
+    @torch.no_grad()
+    def training_epoch_end(self, outputs: list[list[torch.Tensor]]):
+        if (self.current_epoch + 1) % self.config.train.logging_interval != 0:
+            return
+
+        for module in self.generator_ema.modules():
+            if isinstance(module, nn.BatchNorm2d):
+                module.momentum = 0
+                module.reset_running_stats()
+                module.train()
+
+        for _ in range(self.config.train.accumulate_standing_stats):
+            self.generator_ema(*self.sample_random_inputs(truncated=True))
+        self.generator_ema.eval()
+
+        images = self.generator_ema(self.logging_noise_vectors, self.logging_labels)
+        grid = torchvision.utils.make_grid(images, value_range=(-1, 1))
+        self.logger.log_image("val/images", [grid])
+
+    def configure_optimizers(self) -> tuple[dict[str, Any]]:
+        generator_params = self.generator.parameters()
+        discriminator_params = self.discriminator.parameters()
+
+        generator_optimizer = {
+            "optimizer": Adam(generator_params, **self.config.optim.generator),
+            "frequency": 1,
+        }
+        discriminator_optimizer = {
+            "optimizer": Adam(discriminator_params, **self.config.optim.discriminator),
+            "frequency": self.config.optim.num_discriminator_steps,
+        }
+        return generator_optimizer, discriminator_optimizer
+
+
+class BigGANDataModule(LightningDataModule):
+    def __init__(self, config: DictConfig):
+        super().__init__()
+        self.config = config
+
+    def setup(self, stage: Optional[str] = None):
+        dataset = pd.read_csv(self.config.dataset.filename, dtype={"barcode": "str"})
+        dataset = dataset[~dataset.category.isnull()]
+
+        transforms = [
+            A.Resize(
+                self.config.dataset.image_resolution,
+                self.config.dataset.image_resolution,
+            ),
+            A.Normalize(mean=0.5, std=0.5),
+            AP.ToTensorV2(),
+        ]
+
+        self.label2id = {v: k for k, v in enumerate(sorted(dataset.category.unique()))}
+        self.train_dataset = BigGANDataset(
+            dataset,
+            self.config.dataset.imagedir,
+            self.label2id,
+            transform=A.Compose(transforms),
+        )
+
+    def train_dataloader(self) -> DataLoader:
+        num_workers = (
+            self.config.dataset.dataloader_workers
+            if self.config.dataset.dataloader_workers >= 0
+            else os.cpu_count()
+        )
+        return DataLoader(
+            self.train_dataset,
+            batch_size=self.config.train.batch_size,
+            shuffle=True,
+            num_workers=num_workers,
+            pin_memory=True,
+            persistent_workers=True,
+        )
---
+++ bookcover-background-generation/v1/src/modeling.py
@@ -0,0 +1,328 @@
+from dataclasses import dataclass
+from typing import Any, Iterator
+
+import torch
+import torch.nn as nn
+
+
+@dataclass
+class BigGANSelfAttentionConfig:
+    hidden_size: int
+    query_key_size: int
+    value_size: int
+    key_value_pooling: int
+
+
+@dataclass
+class BigGANGeneratorLayerConfig:
+    input_size: int
+    middle_size: int
+    output_size: int
+    conditional_size: int
+    upsampling_factor: int
+
+
+@dataclass
+class BigGANDiscriminatorLayerConfig:
+    input_size: int
+    middle_size: int
+    output_size: int
+    downsampling_factor: int
+
+
+@dataclass
+class BigGANGeneratorConfig:
+    num_classes: int
+    noise_size: int
+    embedding_size: int
+    hidden_size_list: list[int]
+    bottleneck_reduction: int = 4
+    first_hidden_resolution: int = 4
+    num_layers_per_block: int = 2
+    attention_layer_position: int = 7
+    attention_query_key_reduction: int = 8
+    attention_value_reduction: int = 2
+    attention_key_value_pooling: int = 2
+    gradient_checkpointing: bool = False
+
+    @property
+    def first_hidden_size(self) -> int:
+        return self.hidden_size_list[0]
+
+    @property
+    def last_hidden_size(self) -> int:
+        return self.hidden_size_list[-1]
+
+    @property
+    def attention_config(self) -> BigGANSelfAttentionConfig:
+        hidden_size = self.hidden_size_list[self.attention_layer_position]
+        return BigGANSelfAttentionConfig(
+            hidden_size=hidden_size,
+            query_key_size=hidden_size // self.attention_query_key_reduction,
+            value_size=hidden_size // self.attention_value_reduction,
+            key_value_pooling=self.attention_key_value_pooling,
+        )
+
+    def __getitem__(self, idx: int) -> BigGANGeneratorLayerConfig:
+        input_size = self.hidden_size_list[0 if idx == 0 else idx - 1]
+        return BigGANGeneratorLayerConfig(
+            input_size=input_size,
+            middle_size=input_size // self.bottleneck_reduction,
+            output_size=self.hidden_size_list[idx],
+            conditional_size=self.noise_size + self.embedding_size,
+            upsampling_factor=2 if (idx + 1) % self.num_layers_per_block == 0 else 1,
+        )
+
+    def __iter__(self) -> Iterator[BigGANGeneratorLayerConfig]:
+        yield from [self[i] for i, _ in enumerate(self.hidden_size_list)]
+
+
+@dataclass
+class BigGANDiscriminatorConfig:
+    num_classes: int
+    hidden_size_list: list[int]
+    bottleneck_reduction: int = 4
+    num_layers_per_block: int = 2
+    attention_layer_position: int = 5
+    attention_query_key_reduction: int = 8
+    attention_value_reduction: int = 2
+    attention_key_value_pooling: int = 2
+    gradient_checkpointing: bool = False
+
+    @property
+    def first_hidden_size(self) -> int:
+        return self.hidden_size_list[0]
+
+    @property
+    def last_hidden_size(self) -> int:
+        return self.hidden_size_list[-1]
+
+    @property
+    def attention_config(self) -> BigGANSelfAttentionConfig:
+        hidden_size = self.hidden_size_list[self.attention_layer_position]
+        return BigGANSelfAttentionConfig(
+            hidden_size=hidden_size,
+            query_key_size=hidden_size // self.attention_query_key_reduction,
+            value_size=hidden_size // self.attention_value_reduction,
+            key_value_pooling=self.attention_key_value_pooling,
+        )
+
+    def __getitem__(self, idx: int) -> BigGANGeneratorLayerConfig:
+        input_size = self.hidden_size_list[0 if idx == 0 else idx - 1]
+        return BigGANDiscriminatorLayerConfig(
+            input_size=input_size,
+            middle_size=input_size // self.bottleneck_reduction,
+            output_size=self.hidden_size_list[idx],
+            downsampling_factor=2 if idx % self.num_layers_per_block == 0 else 1,
+        )
+
+    def __iter__(self) -> Iterator[BigGANGeneratorLayerConfig]:
+        yield from [self[i] for i, _ in enumerate(self.hidden_size_list)]
+
+
+class CondBatchNorm2d(nn.Module):
+    def __init__(self, hidden_size: int, conditional_size: int):
+        super().__init__()
+        self.batch_norm = nn.BatchNorm2d(hidden_size, affine=False)
+        self.weight = nn.Linear(conditional_size, hidden_size)
+        self.bias = nn.Linear(conditional_size, hidden_size)
+
+    def forward(
+        self, hidden_state: torch.Tensor, conditional_state: torch.Tensor
+    ) -> torch.Tensor:
+        weight, bias = self.weight(conditional_state), self.bias(conditional_state)
+        hidden_state = self.batch_norm(hidden_state)
+        return (1 + weight[:, :, None, None]) * hidden_state + bias[:, :, None, None]
+
+
+class BigGANSelfAttention(nn.Module):
+    def __init__(self, config: BigGANSelfAttentionConfig):
+        super().__init__()
+        self.query = nn.Conv2d(config.hidden_size, config.query_key_size, 1, bias=False)
+        self.key = nn.Conv2d(config.hidden_size, config.query_key_size, 1, bias=False)
+        self.value = nn.Conv2d(config.hidden_size, config.value_size, 1, bias=False)
+
+        self.output = nn.Conv2d(config.value_size, config.hidden_size, 1, bias=False)
+        self.gating = nn.Parameter(torch.zeros(()))
+
+        self.maxpool = (
+            nn.MaxPool2d(config.key_value_pooling, config.key_value_pooling)
+            if config.key_value_pooling > 1
+            else nn.Identity()
+        )
+
+    def forward(self, hidden_state: torch.Tensor) -> torch.Tensor:
+        query = self.query(hidden_state)
+        key = self.maxpool(self.key(hidden_state))
+        value = self.maxpool(self.value(hidden_state))
+
+        attention_probs = torch.matmul(query.flatten(2).transpose(1, 2), key.flatten(2))
+        attention_probs = attention_probs.softmax(-1)
+        attention_probs = attention_probs.transpose(1, 2)
+
+        attention_state = torch.matmul(value.flatten(2), attention_probs)
+        attention_state = attention_state.reshape(value.shape[:2] + query.shape[2:])
+
+        return hidden_state + self.gating * self.output(attention_state)
+
+
+class BigGANGeneratorLayer(nn.Module):
+    def __init__(self, config: BigGANGeneratorLayerConfig):
+        super().__init__()
+        self.conv_1 = nn.Conv2d(config.input_size, config.middle_size, 1)
+        self.conv_2 = nn.Conv2d(config.middle_size, config.middle_size, 3, padding=1)
+        self.conv_3 = nn.Conv2d(config.middle_size, config.middle_size, 3, padding=1)
+        self.conv_4 = nn.Conv2d(config.middle_size, config.output_size, 1)
+
+        self.batch_norm_1 = CondBatchNorm2d(config.input_size, config.conditional_size)
+        self.batch_norm_2 = CondBatchNorm2d(config.middle_size, config.conditional_size)
+        self.batch_norm_3 = CondBatchNorm2d(config.middle_size, config.conditional_size)
+        self.batch_norm_4 = CondBatchNorm2d(config.middle_size, config.conditional_size)
+
+        self.upsampling = (
+            nn.UpsamplingNearest2d(scale_factor=config.upsampling_factor)
+            if config.upsampling_factor > 1
+            else nn.Identity()
+        )
+
+    def forward(
+        self, hidden_state: torch.Tensor, conditional_state: torch.Tensor
+    ) -> torch.Tensor:
+        residual_state = hidden_state
+
+        hidden_state = self.batch_norm_1(hidden_state, conditional_state)
+        hidden_state = self.conv_1(hidden_state.relu())
+
+        hidden_state = self.batch_norm_2(hidden_state, conditional_state)
+        hidden_state = self.upsampling(hidden_state.relu())
+        hidden_state = self.conv_2(hidden_state)
+
+        hidden_state = self.batch_norm_3(hidden_state, conditional_state)
+        hidden_state = self.conv_3(hidden_state.relu())
+
+        hidden_state = self.batch_norm_4(hidden_state, conditional_state)
+        hidden_state = self.conv_4(hidden_state.relu())
+
+        residual_state = residual_state[:, : hidden_state.size(1)]
+        residual_state = self.upsampling(residual_state)
+
+        return residual_state + hidden_state
+
+
+class BigGANDiscriminatorLayer(nn.Module):
+    def __init__(self, config: BigGANDiscriminatorLayerConfig):
+        super().__init__()
+        self.conv_1 = nn.Conv2d(config.input_size, config.middle_size, 1)
+        self.conv_2 = nn.Conv2d(config.middle_size, config.middle_size, 3, padding=1)
+        self.conv_3 = nn.Conv2d(config.middle_size, config.middle_size, 3, padding=1)
+        self.conv_4 = nn.Conv2d(config.middle_size, config.output_size, 1)
+
+        residual_size = config.output_size - config.input_size
+        if residual_size > 0:
+            self.conv_residual = nn.Conv2d(config.input_size, residual_size, 1)
+
+        self.downsampling = (
+            nn.AvgPool2d(config.downsampling_factor, config.downsampling_factor)
+            if config.downsampling_factor > 1
+            else nn.Identity()
+        )
+
+    def forward(self, hidden_state: torch.Tensor) -> torch.Tensor:
+        residual_state = hidden_state
+
+        hidden_state = self.conv_1(hidden_state.relu())
+        hidden_state = self.conv_2(hidden_state.relu())
+        hidden_state = self.conv_3(hidden_state.relu())
+        hidden_state = self.downsampling(hidden_state.relu())
+        hidden_state = self.conv_4(hidden_state)
+
+        residual_state = self.downsampling(residual_state)
+        if residual_state.size(1) != hidden_state.size(1):
+            new_residual = self.conv_residual(residual_state)
+            residual_state = torch.cat((residual_state, new_residual), dim=1)
+
+        return residual_state + hidden_state
+
+
+class BigGANGenerator(nn.Module):
+    def __init__(self, config: BigGANGeneratorConfig):
+        super().__init__()
+        self.config = config
+
+        self.embeddings = nn.Embedding(config.num_classes, config.embedding_size)
+        self.linear = nn.Linear(
+            config.noise_size + config.embedding_size,
+            config.first_hidden_resolution ** 2 * config.first_hidden_size,
+        )
+        self.layers = nn.ModuleList([BigGANGeneratorLayer(cfg) for cfg in config])
+        self.attention = BigGANSelfAttention(config.attention_config)
+
+        self.batch_norm = nn.BatchNorm2d(config.last_hidden_size)
+        self.conv = nn.Conv2d(config.last_hidden_size, 3, 3, padding=1)
+
+        for module in self.modules():
+            if isinstance(module, (nn.Linear, nn.Conv2d, nn.Embedding)):
+                nn.init.orthogonal_(module.weight)
+                nn.utils.parametrizations.spectral_norm(module)
+
+    def forward_module(self, module: nn.Module, *args: Any, **kwargs: Any) -> Any:
+        if self.config.gradient_checkpointing and self.training:
+            return torch.utils.checkpoint.checkpoint(module, *args, **kwargs)
+        return module(*args, **kwargs)
+
+    def forward(
+        self, noise_vectors: torch.Tensor, labels: torch.Tensor
+    ) -> torch.Tensor:
+        conditional_state = torch.cat((noise_vectors, self.embeddings(labels)), dim=1)
+        hidden_state = self.linear(conditional_state).view(
+            conditional_state.size(0),
+            self.config.first_hidden_size,
+            self.config.first_hidden_resolution,
+            self.config.first_hidden_resolution,
+        )
+
+        for i, layer in enumerate(self.layers):
+            hidden_state = self.forward_module(layer, hidden_state, conditional_state)
+            if i == self.config.attention_layer_position:
+                hidden_state = self.forward_module(self.attention, hidden_state)
+
+        hidden_state = self.batch_norm(hidden_state).relu()
+        hidden_state = self.conv(hidden_state)
+        return hidden_state.tanh()
+
+
+class BigGANDiscriminator(nn.Module):
+    def __init__(self, config: BigGANDiscriminatorConfig):
+        super().__init__()
+        self.config = config
+
+        self.conv = nn.Conv2d(3, config.first_hidden_size, 3, padding=1)
+        self.layers = nn.ModuleList([BigGANDiscriminatorLayer(cfg) for cfg in config])
+        self.attention = BigGANSelfAttention(config.attention_config)
+
+        self.embeddings = nn.Embedding(config.num_classes, config.last_hidden_size)
+        self.linear = nn.Linear(config.last_hidden_size, 1)
+
+        for module in self.modules():
+            if isinstance(module, (nn.Linear, nn.Conv2d, nn.Embedding)):
+                nn.init.orthogonal_(module.weight)
+                nn.utils.parametrizations.spectral_norm(module)
+
+    def forward_module(self, module: nn.Module, *args: Any, **kwargs: Any) -> Any:
+        if self.config.gradient_checkpointing and self.training:
+            return torch.utils.checkpoint.checkpoint(module, *args, **kwargs)
+        return module(*args, **kwargs)
+
+    def forward(self, input_images: torch.Tensor, labels: torch.Tensor) -> torch.Tensor:
+        hidden_state = self.conv(input_images)
+
+        for i, layer in enumerate(self.layers):
+            hidden_state = self.forward_module(layer, hidden_state)
+            if i == self.config.attention_layer_position:
+                hidden_state = self.forward_module(self.attention, hidden_state)
+
+        hidden_state = hidden_state.sum((2, 3))
+        output_logits = (self.embeddings(labels) * hidden_state).sum(-1)
+        output_logits = output_logits + self.linear(hidden_state).squeeze(-1)
+
+        return output_logits
