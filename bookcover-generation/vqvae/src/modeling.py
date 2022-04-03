a60446a06fa2dace36eabbbbf2e63b95b2f05f08
affjljoo3581 <affjljoo3581@gmail.com>
2022-04-04 00:25:38 +0900

    add initial vqvae project

---
+++ bookcover-generation/vqvae/src/modeling.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass
+
+import torch.nn as nn
+
+
+@dataclass
+class VQVAEncoderLayerConfig:
+    ...
+
+
+@dataclass
+class VQVAEDecoderLayerConfig:
+    ...
+
+
+@dataclass
+class VQVAEQuantizerConfig:
+    ...
