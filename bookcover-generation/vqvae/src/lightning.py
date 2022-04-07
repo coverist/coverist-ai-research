cfbe62342fa1bc2e609837382f5acb6d25e33aaf
affjljoo3581 <affjljoo3581@gmail.com>
2022-04-08 03:12:55 +0900

    change to make width of grid even

--- bookcover-generation/vqvae/src/lightning.py
+++ bookcover-generation/vqvae/src/lightning.py
@@ -74,7 +74,8 @@ class VQVAETrainingModule(LightningModule):
 
     def validation_epoch_end(self, outputs: list[tuple[torch.Tensor, torch.Tensor]]):
         images = torch.stack(outputs[0], dim=1).flatten(0, 1)
-        images = make_grid(images, int(images.size(0) ** 0.5), value_range=(-1, 1))
+        nrow = int(images.size(0) ** 0.5) // 2 * 2
+        images = make_grid(images, nrow, value_range=(-1, 1))
         self.logger.log_image("val/reconstruct", [images])
 
     def configure_optimizers(self) -> tuple[list[Optimizer], list[LRScheduler]]:
