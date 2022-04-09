a0d8b3497ccff72b629a3e92a0e570e21c9b1e84
affjljoo3581 <affjljoo3581@gmail.com>
2022-04-09 16:15:28 +0900

    add ocr loss

--- bookcover-generation/vqvae/src/lightning.py
+++ bookcover-generation/vqvae/src/lightning.py
@@ -2,8 +2,9 @@ import math
 import os
 from typing import Any, Optional
 
+import easyocr
 import torch
-import torch.nn as nn
+import torch.nn.functional as F
 from omegaconf import DictConfig
 from pytorch_lightning import LightningDataModule, LightningModule
 from sklearn.model_selection import train_test_split
@@ -32,16 +33,29 @@ class VQVAETrainingModule(LightningModule):
 
         self.encoder = VQVAEEncoder(VQVAEEncoderConfig(**config.model.encoder))
         self.decoder = VQVAEDecoder(VQVAEDecoderConfig(**config.model.decoder))
-        self.criterion = nn.L1Loss()
 
-    def forward(self, images: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor]:
-        reconstructed = self.decoder(self.encoder(images))
-        loss = self.criterion(images, reconstructed)
-        return reconstructed, loss
+        self.ocr = easyocr.Reader(["ko"]).detector.module.basenet
+        self.ocr.requires_grad_(False)
+
+    def forward(self, images: torch.Tensor) -> tuple[torch.Tensor, ...]:
+        recon = self.decoder(self.encoder(images))
+        loss_recon = F.l1_loss(images, recon)
+
+        self.ocr.eval()
+        loss_ocr_list = [
+            F.mse_loss(ocr_images, ocr_recon)
+            for ocr_images, ocr_recon in zip(self.ocr(images), self.ocr(recon))
+        ]
+        loss_ocr = sum(loss_ocr_list) / len(loss_ocr_list)
+
+        loss = loss_recon + loss_ocr
+        return recon, loss, loss_recon, loss_ocr
 
     def training_step(self, images: torch.Tensor, batch_idx: int) -> torch.Tensor:
-        _, loss = self(images)
+        _, loss, loss_recon, loss_ocr = self(images)
         self.log("train/loss", loss)
+        self.log("train/loss_recon", loss_recon)
+        self.log("train/loss_ocr", loss_ocr)
         self.log("train/temperature", self.encoder.temperature)
         self.log("step", self.global_step)
         return loss
@@ -49,10 +63,12 @@ class VQVAETrainingModule(LightningModule):
     def validation_step(
         self, images: torch.Tensor, batch_idx: int
     ) -> tuple[torch.Tensor, torch.Tensor]:
-        reconstructed, loss = self(images)
+        recon, loss, loss_recon, loss_ocr = self(images)
         self.log("val/loss", loss)
+        self.log("val/loss_recon", loss_recon)
+        self.log("val/loss_ocr", loss_ocr)
         self.log("step", self.global_step)
-        return images, reconstructed
+        return images, recon
 
     def validation_epoch_end(self, outputs: list[tuple[torch.Tensor, torch.Tensor]]):
         images = torch.stack(outputs[0], dim=1).flatten(0, 1)
@@ -64,7 +80,10 @@ class VQVAETrainingModule(LightningModule):
         self.logger.log_image("val/reconstructed", [images])
 
     def configure_optimizers(self) -> tuple[list[Optimizer], list[LRScheduler]]:
-        optimizer = Adam(self.parameters(), **self.config.optim.optimizer)
+        optimizer = Adam(
+            list(self.encoder.parameters()) + list(self.decoder.parameters()),
+            **self.config.optim.optimizer
+        )
         scheduler = CosineAnnealingLR(optimizer, self.config.train.epochs)
         return [optimizer], [scheduler]
 
