1713b4c6195aa1d3099b6a0941d279e5cd13bc37
affjljoo3581 <affjljoo3581@gmail.com>
2022-04-16 01:47:02 +0900

    fix bugs

--- bookcover-generation/vqgan/src/modeling.py
+++ bookcover-generation/vqgan/src/modeling.py
@@ -171,7 +171,9 @@ class VQVAEQuantizer(nn.Embedding):
             new_embeddings = new_embeddings * (embedding_usages[:, None] > 0).float()
 
             new_embeddings.scatter_add_(
-                0, flatten_indices, encoded.permute(0, 2, 3, 1).flatten(0, 2).float()
+                0,
+                flatten_indices[:, None],
+                encoded.permute(0, 2, 3, 1).flatten(0, 2).float(),
             )
             new_embeddings = new_embeddings / embedding_usages[:, None].clamp(1)
 
