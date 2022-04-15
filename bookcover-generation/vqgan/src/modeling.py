79b501916c67e3312aa6d1a561cedfb07c04b97b
affjljoo3581 <affjljoo3581@gmail.com>
2022-04-16 02:02:09 +0900

    fix bugs

--- bookcover-generation/vqgan/src/modeling.py
+++ bookcover-generation/vqgan/src/modeling.py
@@ -143,7 +143,7 @@ class VQVAEDecoder(nn.Module):
 class VQVAEQuantizer(nn.Embedding):
     def __init__(self, config: VQVAEQuantizerConfig):
         super().__init__(config.num_embeddings, config.embedding_dim)
-        nn.init.orthogonal_(self.weight, std=config.initialize_scale)
+        nn.init.orthogonal_(self.weight, gain=config.initialize_scale)
 
     def forward(
         self, encoded: torch.Tensor
