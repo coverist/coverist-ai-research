25ddbf22f079780b41e7547da165ba7818909370
affjljoo3581 <affjljoo3581@gmail.com>
2022-04-28 15:44:38 +0900

    update the project codes

---
+++ bookcover-generation-v2/clip/src/dataset.py
@@ -0,0 +1,88 @@
+import os
+import random
+from dataclasses import dataclass
+from typing import Any, Callable
+
+import albumentations as A
+import cv2
+import pandas as pd
+import torch
+from albumentations.pytorch import ToTensorV2
+from torch.utils.data import Dataset
+from transformers import DataCollatorWithPadding, PreTrainedTokenizerBase
+
+
+class CLIPTransform(A.Compose):
+    def __init__(self, image_size: int, augmentation: bool = True):
+        transforms = [
+            A.Resize(image_size, image_size),
+            A.HueSaturationValue(
+                hue_shift_limit=20,
+                sat_shift_limit=30,
+                val_shift_limit=20,
+                p=0.5 if augmentation else 0.0,
+            ),
+            A.ShiftScaleRotate(
+                shift_limit=0.1,
+                scale_limit=0.1,
+                rotate_limit=15,
+                border_mode=cv2.BORDER_CONSTANT,
+                value=0x7F,
+                p=0.5 if augmentation else 0.0,
+            ),
+            A.Cutout(
+                num_holes=1,
+                max_h_size=96,
+                max_w_size=96,
+                fill_value=0x7F,
+                p=0.2 if augmentation else 0.0,
+            ),
+            A.Normalize(0.5, 0.5),
+            ToTensorV2(),
+        ]
+        super().__init__(transforms)
+
+
+@dataclass
+class BookCoverPairedDataset(Dataset):
+    dataset: pd.DataFrame
+    image_dir: str
+    max_length: int
+    transform: Callable
+    tokenizer: PreTrainedTokenizerBase
+
+    def __len__(self) -> int:
+        return len(self.dataset)
+
+    def __getitem__(self, index: int) -> tuple[torch.Tensor, dict[str, Any]]:
+        example = self.dataset.iloc[index]
+
+        # Read the book-cover image and return other example if the image is invalid.
+        path = os.path.join(self.image_dir, *example.isbn[-3], f"{example.isbn}.jpg")
+        image = cv2.imread(path)
+        if image is None:
+            return self[random.randint(0, len(self))]
+
+        # Change the color format of the image and apply image augmentations.
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        image = self.transform(image=image)["image"]
+
+        # Create the text query prompt and tokenize with truncation.
+        text_queries = [example.title, example.author, example.publisher]
+        text_encoding = self.tokenizer(
+            f" {self.tokenizer.sep_token} ".join(text_queries),
+            truncation=True,
+            max_length=self.max_length,
+        )
+        return image, text_encoding
+
+
+@dataclass
+class DataCollatorForImageTextPair(DataCollatorWithPadding):
+    def __call__(
+        self, features: list[tuple[torch.Tensor, dict[str, Any]]]
+    ) -> tuple[torch.Tensor, dict[str, torch.Tensor]]:
+        images = [feature[0] for feature in features]
+        encodings = [features[1] for feature in features]
+
+        return torch.stack(images), super().__call__(encodings)
---
+++ bookcover-generation-v2/clip/src/lightning.py
@@ -0,0 +1,147 @@
+import os
+from typing import Any, Optional
+
+import pandas as pd
+import timm
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from omegaconf import DictConfig
+from pytorch_lightning import LightningDataModule, LightningModule
+from sklearn.model_selection import train_test_split
+from torch.optim import Optimizer
+from torch.utils.data import DataLoader
+from transformers import AutoModel, AutoTokenizer, get_scheduler
+
+from dataset import BookCoverPairedDataset, CLIPTransform, DataCollatorForImageTextPair
+
+try:
+    from apex.optimizers import FusedAdam as AdamW
+except ModuleNotFoundError:
+    from torch.optim import AdamW
+
+
+class CLIPTrainingModule(LightningModule):
+    def __init__(self, config: DictConfig):
+        super().__init__()
+        self.config = config
+        self.image_encoder = timm.create_model(
+            config.model.image_encoder, pretrained=True, num_classes=0
+        )
+        self.image_projection = nn.Linear(
+            self.image_encoder.num_features,
+            config.model.embedding_dim,
+        )
+        self.text_encoder = AutoModel.from_pretrained(config.model.text_encoder)
+        self.text_projection = nn.Linear(
+            self.text_encoder.config.hidden_size,
+            config.model.embedding_dim,
+        )
+        self.temperature = nn.Parameter(torch.tensor(2.6593))
+
+    def forward(
+        self,
+        input_images: torch.Tensor,
+        input_texts: dict[str, torch.Tensor],
+    ) -> tuple[torch.Tensor, torch.Tensor]:
+        # Get image embeddings and text embeddings to calculate cosine similarity.
+        image_features = self.image_projection(self.image_encoder(input_images))
+        image_features = F.normalize(image_features, eps=1e-6)
+
+        text_features = self.text_projection(self.text_encoder(**input_texts)[0][:, 0])
+        text_features = F.normalize(text_features, eps=1e-6)
+
+        # Calculate the cosine similarity and scale the logits.
+        logits = torch.matmul(image_features, text_features.transpose(0, 1))
+        logits = logits * self.temperature.exp().clamp_max(100)
+
+        # Calculate the cross-entropy loss and accuracy. Note that the images and texts
+        # are paired across batch axis, thus the labels should be an identity mapping.
+        labels = torch.arange(logits.size(0), device=logits.device)
+        loss = F.cross_entropy(logits, labels)
+        accuracy = (logits.argmax(dim=1) == labels).float().mean()
+        return loss, accuracy
+
+    def training_step(self, batch: tuple[torch.Tensor, ...], idx: int) -> torch.Tensor:
+        loss, accuracy = self(*batch)
+        self.log("train/loss", loss)
+        self.log("train/accuracy", accuracy)
+        self.log("step", self.global_step)
+        return loss
+
+    def validation_step(self, batch: tuple[torch.Tensor, ...], idx: int):
+        loss, accuracy = self(*batch)
+        self.log("val/loss", loss)
+        self.log("val/accuracy", accuracy)
+        self.log("step", self.global_step)
+
+    def get_parameter_groups(self) -> list[dict[str, Any]]:
+        do_decay = [p for p in self.model.parameters() if p.ndim < 2]
+        no_decay = [p for p in self.model.parameters() if p.ndim >= 2]
+        return [{"params": do_decay}, {"params": no_decay, "weight_decay": 0.0}]
+
+    def configure_optimizers(self) -> tuple[list[Optimizer], list[dict[str, Any]]]:
+        optimizer = AdamW(self.get_parameter_groups(), **self.config.optim.optimizer)
+        scheduler = get_scheduler(optimizer=optimizer, **self.config.optim.scheduler)
+        return [optimizer], [{"scheduler": scheduler, "interval": "step"}]
+
+    def on_load_checkpoint(self, checkpoint: dict[str, Any]):
+        if "ApexMixedPrecisionPlugin" in checkpoint:
+            checkpoint.pop("ApexMixedPrecisionPlugin")
+
+
+class CLIPDataModule(LightningDataModule):
+    def __init__(self, config: DictConfig):
+        super().__init__()
+        self.config = config
+
+    def setup(self, stage: Optional[str] = None):
+        tokenizer = AutoTokenizer.from_pretrained(self.config.model.text_encoder)
+
+        dataset = pd.read_json(self.config.data.dataset, lines=True, dtype=None)
+        dataset = dataset[dataset.with_cover]
+        dataset = dataset[dataset.cover_aspect_ratio > 0.5]
+        dataset = dataset[dataset.cover_aspect_ratio < 0.9]
+
+        train_dataset, val_dataset = train_test_split(
+            dataset,
+            test_size=self.config.data.validation_ratio,
+            random_state=42,
+            shuffle=True,
+        )
+        self.train_dataset = BookCoverPairedDataset(
+            train_dataset,
+            image_dir=self.config.data.image_dir,
+            max_length=self.config.data.max_length,
+            transform=CLIPTransform(self.config.data.image_size, augmentation=True),
+            tokenizer=tokenizer,
+        )
+        self.val_dataset = BookCoverPairedDataset(
+            val_dataset,
+            image_dir=self.config.data.image_dir,
+            max_length=self.config.data.max_length,
+            transform=CLIPTransform(self.config.data.image_size, augmentation=False),
+            tokenizer=tokenizer,
+        )
+        self.collator = DataCollatorForImageTextPair(tokenizer, pad_to_multiple_of=8)
+
+    def train_dataloader(self) -> DataLoader:
+        return DataLoader(
+            self.train_dataset,
+            batch_size=self.config.train.batch_size,
+            shuffle=True,
+            num_workers=os.cpu_count(),
+            collate_fn=self.collator,
+            pin_memory=True,
+            persistent_workers=True,
+        )
+
+    def val_dataloader(self) -> DataLoader:
+        return DataLoader(
+            self.val_dataset,
+            batch_size=self.config.train.batch_size,
+            num_workers=os.cpu_count(),
+            collate_fn=self.collator,
+            pin_memory=True,
+            persistent_workers=True,
+        )
