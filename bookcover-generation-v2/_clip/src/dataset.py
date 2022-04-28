30fc80e6e1a50ae6384ee9d7317309d3a5936302
affjljoo3581 <affjljoo3581@gmail.com>
2022-04-29 02:28:43 +0900

    update the code

--- bookcover-generation-v2/_clip/src/dataset.py
+++ bookcover-generation-v2/_clip/src/dataset.py
@@ -1,7 +1,7 @@
 import os
 import random
 from dataclasses import dataclass
-from typing import Any, Callable, Optional
+from typing import Any, Callable
 
 import albumentations as A
 import cv2
@@ -49,7 +49,6 @@ class BookCoverPairedDataset(Dataset):
     dataset: pd.DataFrame
     image_dir: str
     max_length: int
-    negative_samples: int
     transform: Callable
     tokenizer: PreTrainedTokenizerBase
 
@@ -59,50 +58,26 @@ class BookCoverPairedDataset(Dataset):
     def __len__(self) -> int:
         return len(self.dataset)
 
-    def read_image_and_transform(self, example: pd.Series) -> Optional[torch.Tensor]:
+    def __getitem__(self, index: int) -> tuple[torch.Tensor, dict[str, Any]]:
+        example = self.dataset.iloc[index]
         path = os.path.join(self.image_dir, *example.isbn[-3:], f"{example.isbn}.jpg")
+
+        # Read the book-cover image and apply augmentations.
         try:
             with open(path, "rb") as fp:
                 image = self.turbojpeg.decode(fp.read(), pixel_format=TJCS_RGB)
-                return self.transform(image=image)["image"]
+                image = self.transform(image=image)["image"]
         except OSError:
-            # Sometimes when the image is truncated, turbojpeg will throw `OSError`.
-            # This function will return `None` to notify that the current example has
-            # invalid image.
-            return None
+            return self[random.randint(0, len(self))]
 
-    def create_text_encoding(
-        self, example: pd.Series, negative: bool = False
-    ) -> dict[str, Any]:
-        queries = [example.title, example.author, example.publisher]
-        negative_queries, target_index = queries.copy(), random.randint(0, len(queries))
-
-        # Create new negative sample which has different query from the original one.
-        # Note that if `negative=False` then nothing will be replaced.
-        while negative and negative_queries[target_index] == queries[target_index]:
-            alternatvie = self.dataset.sample(1).iloc[0]
-            alternatvie = [alternatvie.title, alternatvie.author, alternatvie.publisher]
-            negative_queries[target_index] = alternatvie[target_index]
-
-        return self.tokenizer(
-            f" {self.tokenizer.sep_token} ".join(negative_queries),
+        # Create the text query prompt and tokenize with truncation.
+        text_queries = [example.title, example.author, example.publisher]
+        text_encoding = self.tokenizer(
+            f" {self.tokenizer.sep_token} ".join(text_queries),
             truncation=True,
             max_length=self.max_length,
         )
-
-    def __getitem__(self, index: int) -> tuple[torch.Tensor, list[dict[str, Any]]]:
-        example = self.dataset.iloc[index]
-        image = self.read_image_and_transform(example)
-        if image is None:
-            return self[random.randint(0, len(self))]
-
-        # Create one positive text sample and several negative samples. All samples will
-        # be merged and compared with the images.
-        text_encodings = [
-            self.create_text_encoding(example, negative=i > 0)
-            for i in range(1 + self.negative_samples)
-        ]
-        return image, text_encodings
+        return image, text_encoding
 
 
 @dataclass
@@ -111,6 +86,6 @@ class DataCollatorForImageTextPair(DataCollatorWithPadding):
         self, features: list[tuple[torch.Tensor, dict[str, Any]]]
     ) -> tuple[torch.Tensor, dict[str, torch.Tensor]]:
         images = [feature[0] for feature in features]
-        encodings = [encoding for feature in features for encoding in feature[1]]
+        encodings = [feature[1] for feature in features]
 
         return torch.stack(images), dict(super().__call__(encodings))
--- bookcover-generation-v2/_clip/src/lightning.py
+++ bookcover-generation-v2/_clip/src/lightning.py
@@ -29,8 +29,6 @@ class CLIPTrainingModule(LightningModule):
     def __init__(self, config: DictConfig):
         super().__init__()
         self.config = config
-        self.negative_samples = config.data.negative_samples
-
         self.image_encoder = timm.create_model(
             config.model.image_encoder,
             pretrained=True,
@@ -42,8 +40,6 @@ class CLIPTrainingModule(LightningModule):
         )
         self.temperature = nn.Parameter(torch.tensor(2.6593))
 
-        # If `use_gradient_checkpoint=True` then enable the gradient checkpointing to
-        # the image-encoder and text-encoder model to save the GPU memory usage.
         if config.train.use_gradient_checkpoint:
             self.image_encoder.set_grad_checkpointing()
             self.text_encoder.gradient_checkpointing_enable()
@@ -52,7 +48,7 @@ class CLIPTrainingModule(LightningModule):
         self,
         input_images: torch.Tensor,
         input_texts: dict[str, torch.Tensor],
-    ) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
+    ) -> tuple[torch.Tensor, torch.Tensor]:
         # Get image embeddings and text embeddings to calculate cosine similarity.
         image_features = F.normalize(self.image_encoder(input_images), eps=1e-6)
         text_features = F.normalize(self.text_encoder(**input_texts)[0], eps=1e-6)
@@ -61,43 +57,24 @@ class CLIPTrainingModule(LightningModule):
         logits = torch.matmul(image_features, text_features.transpose(0, 1))
         logits = logits * self.temperature.exp().clamp_max(100)
 
-        # Calculate cross-entropy loss and accuracy for entire correctness of
-        # predictions. Note that each image example has one positive text sample and
-        # several negative samples.
-        labels = torch.arange(
-            start=0,
-            end=logits.size(1),
-            step=1 + self.negative_samples,
-            device=logits.device,
-        )
+        # Calculate the cross-entropy loss and accuracy. Note that the images and texts
+        # are paired across batch axis, thus the labels should be an identity mapping.
+        labels = torch.arange(logits.size(0), device=logits.device)
         loss = F.cross_entropy(logits, labels)
         accuracy = (logits.argmax(dim=1) == labels).float().mean()
-
-        # Compute the inter-group accuracy and groupwise accuracy.
-        grouped_logits = logits.unflatten(-1, (-1, 1 + self.negative_samples))
-        groupwise_accuracy = (grouped_logits.argmax(dim=2) == 0).float().mean()
-
-        labels = torch.arange(0, logits.size(0), device=logits.device)
-        intergroup_accuracy = grouped_logits.max(dim=2).values.argmax(dim=1) == labels
-        intergroup_accuracy = intergroup_accuracy.float().mean()
-
-        return loss, accuracy, intergroup_accuracy, groupwise_accuracy
+        return loss, accuracy
 
     def training_step(self, batch: tuple[torch.Tensor, ...], idx: int) -> torch.Tensor:
-        loss, accuracy, intergroup_accuracy, groupwise_accuracy = self(*batch)
+        loss, accuracy = self(*batch)
         self.log("train/loss", loss)
         self.log("train/accuracy", accuracy)
-        self.log("train/intergroup_accuracy", intergroup_accuracy)
-        self.log("train/groupwise_accuracy", groupwise_accuracy)
         self.log("step", self.global_step)
         return loss
 
     def validation_step(self, batch: tuple[torch.Tensor, ...], idx: int):
-        loss, accuracy, intergroup_accuracy, groupwise_accuracy = self(*batch)
+        loss, accuracy = self(*batch)
         self.log("val/loss", loss)
         self.log("val/accuracy", accuracy)
-        self.log("val/intergroup_accuracy", intergroup_accuracy)
-        self.log("val/groupwise_accuracy", groupwise_accuracy)
         self.log("step", self.global_step)
 
     def get_parameter_groups(self) -> list[dict[str, Any]]:
@@ -138,7 +115,6 @@ class CLIPDataModule(LightningDataModule):
             train_dataset,
             image_dir=self.config.data.image_dir,
             max_length=self.config.data.max_length,
-            negative_samples=self.config.data.negative_samples,
             transform=CLIPTransform(self.config.data.image_size, augmentation=True),
             tokenizer=tokenizer,
         )
@@ -146,7 +122,6 @@ class CLIPDataModule(LightningDataModule):
             val_dataset,
             image_dir=self.config.data.image_dir,
             max_length=self.config.data.max_length,
-            negative_samples=self.config.data.negative_samples,
             transform=CLIPTransform(self.config.data.image_size, augmentation=False),
             tokenizer=tokenizer,
         )
