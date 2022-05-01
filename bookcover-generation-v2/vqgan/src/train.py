8c9e760f7c46194bf040f417d5d3f7ffdb8bb5ff
affjljoo3581 <affjljoo3581@gmail.com>
2022-05-01 21:03:22 +0900

    implement new vqgan model

--- bookcover-generation-v2/vqgan/src/train.py
+++ bookcover-generation-v2/vqgan/src/train.py
@@ -1,17 +1,16 @@
 import argparse
-import os
 import warnings
 from typing import Optional
 
 from omegaconf import DictConfig, OmegaConf
 from pytorch_lightning import Trainer
-from pytorch_lightning.callbacks import LearningRateMonitor, ModelCheckpoint
+from pytorch_lightning.callbacks import ModelCheckpoint
 from pytorch_lightning.loggers import WandbLogger
 
-from lightning import CLIPDataModule, CLIPTrainingModule
+from dataset import create_train_val_dataloaders
+from lightning import VQGANTrainingModule
 
 warnings.filterwarnings("ignore")
-os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
 
 def main(
@@ -24,21 +23,21 @@ def main(
         precision=16,
         amp_backend="apex",
         log_every_n_steps=config.train.log_every_n_steps,
-        max_steps=config.optim.scheduler.num_training_steps,
+        max_epochs=config.train.epochs,
         gradient_clip_val=config.train.gradient_clip_val,
         accumulate_grad_batches=config.train.accumulate_grad_batches,
         val_check_interval=min(config.train.validation_interval, 1.0),
         check_val_every_n_epoch=max(int(config.train.validation_interval), 1),
-        callbacks=[ModelCheckpoint(save_last=True), LearningRateMonitor("step")],
+        callbacks=[ModelCheckpoint(save_last=True)],
         logger=WandbLogger(
-            project="bookcover-generation-v2-clip",
+            project="bookcover-generation-v2-vqgan",
             name=config.train.name,
             id=resume_id,
         ),
     )
     trainer.fit(
-        CLIPTrainingModule(config),
-        CLIPDataModule(config),
+        VQGANTrainingModule(config),
+        *create_train_val_dataloaders(config),
         ckpt_path=resume_from,
     )
 
