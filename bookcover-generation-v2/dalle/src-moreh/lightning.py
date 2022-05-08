3465dee2e8af8d307209f38436b4499f2c5915ed
affjljoo3581 <affjljoo3581@gmail.com>
2022-05-08 21:32:44 +0900

    add codes for moreh-compatible

---
+++ bookcover-generation-v2/dalle/src-moreh/lightning.py
@@ -0,0 +1,141 @@
+import os
+from typing import Any, Dict, List, Optional, Tuple
+
+import pandas as pd
+import torch
+from omegaconf import DictConfig
+from pytorch_lightning import LightningDataModule, LightningModule
+from sklearn.model_selection import train_test_split
+from torch.optim import Optimizer
+from torch.utils.data import DataLoader, Subset
+from transformers import (
+    AutoTokenizer,
+    BertConfig,
+    BertLMHeadModel,
+    DataCollatorForSeq2Seq,
+    EncoderDecoderModel,
+    get_scheduler,
+)
+
+from dataset import DALLEBookDataset
+from modeling import VQGANDecoder
+
+try:
+    from apex.optimizers import FusedAdam as AdamW
+except ModuleNotFoundError:
+    from torch.optim import AdamW
+
+
+class DALLETrainingModule(LightningModule):
+    def __init__(self, config: DictConfig):
+        super().__init__()
+        self.config = config
+
+        self.model = EncoderDecoderModel.from_encoder_decoder_pretrained(
+            config.model.encoder,
+            decoder_model=BertLMHeadModel(BertConfig(**config.model.decoder)),
+        )
+        self.model.config.decoder_start_token_id = config.model.decoder.bos_token_id
+        self.model.config.eos_token_id = config.model.decoder.eos_token_id
+        self.model.config.pad_token_id = config.model.decoder.bos_token_id
+        self.model.config.vocab_size = config.model.decoder.vocab_size
+
+        self.vqgan = VQGANDecoder.from_pretrained(config.model.vqgan)
+        self.tokenizer = AutoTokenizer.from_pretrained(config.model.encoder)
+
+    def training_step(self, batch: Dict[str, torch.Tensor], idx: int) -> torch.Tensor:
+        loss = self.model(**batch).loss
+        self.log("train/loss", loss)
+        self.log("step", self.global_step)
+        return loss
+
+    def validation_step(
+        self, batch: Dict[str, torch.Tensor], idx: int
+    ) -> Dict[str, torch.Tensor]:
+        self.log("val/loss", self.model(**batch).loss)
+        return batch
+
+    def validation_epoch_end(self, batch_list: List[Dict[str, torch.Tensor]]):
+        # Generate the image sequences using given descriptions.
+        outputs = self.model.generate(
+            batch_list[0]["input_ids"][:32],
+            attention_mask=batch_list[0]["attention_mask"][:32],
+            **self.config.model.generation
+        )
+        outputs = outputs[:, 1:].unflatten(1, (int(outputs.size(1) ** 0.5), -1))
+
+        self.logger.log_image(
+            "val/generated",
+            images=list(self.vqgan(outputs)),
+            caption=self.tokenizer.batch_decode(batch_list[0]["input_ids"][:32], True),
+        )
+
+    def get_parameter_groups(self) -> List[Dict[str, Any]]:
+        do_decay = [p for p in self.model.parameters() if p.ndim < 2]
+        no_decay = [p for p in self.model.parameters() if p.ndim >= 2]
+        return [{"params": do_decay}, {"params": no_decay, "weight_decay": 0.0}]
+
+    def configure_optimizers(self) -> Tuple[List[Optimizer], List[Dict[str, Any]]]:
+        optimizer = AdamW(self.get_parameter_groups(), **self.config.optim.optimizer)
+        scheduler = get_scheduler(optimizer=optimizer, **self.config.optim.scheduler)
+        return [optimizer], [{"scheduler": scheduler, "interval": "step"}]
+
+    def on_load_checkpoint(self, checkpoint: Dict[str, Any]):
+        if "ApexMixedPrecisionPlugin" in checkpoint:
+            checkpoint.pop("ApexMixedPrecisionPlugin")
+
+
+class DALLETrainingDataModule(LightningDataModule):
+    def __init__(self, config: DictConfig):
+        super().__init__()
+        self.config = config
+
+    def setup(self, stage: Optional[str] = None):
+        tokenizer = AutoTokenizer.from_pretrained(self.config.model.encoder)
+
+        # Load a book dataset and filter invalid cover samples (e.g. too low or too high
+        # aspect ratio).
+        books = pd.read_json(self.config.data.book_dataset, lines=True, dtype=False)
+        books = books[books.with_cover]
+        books = books[books.cover_aspect_ratio > 0.5]
+        books = books[books.cover_aspect_ratio < 0.9]
+
+        # Load an image dataset and create the dataset for pairing text descriptions and
+        # images.
+        images = pd.read_csv(self.config.data.image_dataset, dtype={"id": str})
+        images = images.set_index("id")
+
+        dataset = DALLEBookDataset(
+            tokenizer, books, images, max_length=self.config.data.text_max_length
+        )
+
+        # Split the dataset into train and validation.
+        train_indices, val_indices = train_test_split(
+            range(len(dataset)),
+            test_size=self.config.data.validation_ratio,
+            random_state=42,
+            shuffle=True,
+        )
+        self.train_dataset = Subset(dataset, train_indices)
+        self.val_dataset = Subset(dataset, val_indices)
+        self.collator = DataCollatorForSeq2Seq(tokenizer, pad_to_multiple_of=8)
+
+    def train_dataloader(self) -> DataLoader:
+        return DataLoader(
+            self.train_dataset,
+            self.config.train.batch_size,
+            shuffle=True,
+            num_workers=os.cpu_count(),
+            collate_fn=self.collator,
+            persistent_workers=True,
+        )
+
+    def val_dataloader(self) -> DataLoader:
+        return DataLoader(
+            self.val_dataset,
+            self.config.train.batch_size,
+            shuffle=True,
+            num_workers=os.cpu_count(),
+            collate_fn=self.collator,
+            persistent_workers=True,
+        )
