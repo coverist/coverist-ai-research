49e3fd263e333ffb9a86a1362aa6a368d26e1a22
affjljoo3581 <affjljoo3581@gmail.com>
2022-05-21 18:17:55 +0900

    return to the original implementation

--- bookcover-generation-v3/vqgan/src/modeling.py
+++ bookcover-generation-v3/vqgan/src/modeling.py
@@ -46,8 +46,7 @@ class VQGANEncoder(nn.Module):
     @torch.no_grad()
     def init_weights(self, module: Optional[nn.Module] = None):
         if module is None:
-            self.stem.apply(self.init_weights)
-            self.blocks.apply(self.init_weights)
+            self.apply(self.init_weights)
         elif isinstance(module, nn.Conv2d):
             nn.init.orthogonal_(module.weight)
             nn.utils.parametrizations.spectral_norm(module)
@@ -87,8 +86,7 @@ class VQGANDecoder(nn.Module):
     @torch.no_grad()
     def init_weights(self, module: Optional[nn.Module] = None):
         if module is None:
-            self.stem.apply(self.init_weights)
-            self.blocks.apply(self.init_weights)
+            self.apply(self.init_weights)
         elif isinstance(module, nn.Conv2d):
             nn.init.orthogonal_(module.weight)
             nn.utils.parametrizations.spectral_norm(module)
@@ -158,6 +156,15 @@ class PatchDiscriminator(nn.Sequential):
             nn.LeakyReLU(0.2),
             nn.Conv2d(8 * base_dim, 1, kernel_size=4, padding=1),
         )
+        self.init_weights()
+
+    @torch.no_grad()
+    def init_weights(self, module: Optional[nn.Module] = None):
+        if module is None:
+            self.apply(self.init_weights)
+        elif isinstance(module, nn.Conv2d):
+            nn.init.orthogonal_(module.weight)
+            nn.utils.parametrizations.spectral_norm(module)
 
 
 class PerceptualLoss(nn.Module):
