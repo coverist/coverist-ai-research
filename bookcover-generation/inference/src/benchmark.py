7a403b0e7201747383c69deed47dc6755bbc1833
affjljoo3581 <affjljoo3581@gmail.com>
2022-05-29 23:43:19 +0900

    update the code

--- bookcover-generation/inference/src/benchmark.py
+++ bookcover-generation/inference/src/benchmark.py
@@ -31,11 +31,6 @@ def main(args: argparse.Namespace):
     jit_model = torch.jit.load(args.jit_model)
     tokenizer = AutoTokenizer.from_pretrained(args.tokenizer)
 
-    if args.use_fp16:
-        print("[*] Convert the model to `torch.float16`")
-        torch_model.half()
-        jit_model.half()
-
     for i, test_case in enumerate(GENERATION_TEST_CASES):
         encodings = f" {tokenizer.sep_token} ".join(test_case)
         encodings = tokenizer(encodings, return_tensors="pt")
@@ -79,5 +74,4 @@ if __name__ == "__main__":
     parser.add_argument("--jit-model", default="dalle-jit.pth")
     parser.add_argument("--tokenizer", default="dalle-tokenizer")
     parser.add_argument("--use-gpu", action="store_true", default=False)
-    parser.add_argument("--use-fp16", action="store_true", default=False)
     main(parser.parse_args())
